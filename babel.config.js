025854fb51aa9b782189843eea246dd9bc27d666
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-12-29 19:44:35 -0800

    Updating the repository based on a modern look at libraries

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,34 @@
 module.exports = {
-  "sourceMaps": "inline",
   presets: [
     "@babel/preset-typescript"
   ],
   plugins: [
     "@babel/plugin-proposal-object-rest-spread",
+    ["@babel/plugin-proposal-pipeline-operator", {"proposal": "fsharp"}]
   ],
   env: {
     main: {
       presets: [
-        ["@babel/preset-env", {useBuiltIns: "usage", corejs: "3.6", targets: {"node": 10}}],
+        ["@babel/preset-env", {"modules": "cjs", useBuiltIns: "usage", corejs: 3, targets: {"node": 14}}],
+        "minify"
+      ]
+    },
+    module: {
+      presets: [
+        ["@babel/preset-env", {"modules": false, useBuiltIns: "usage", corejs: 3, targets: {"node": 14}}],
         "minify"
       ]
     },
     browser: {
+      sourceMaps: "inline",
       presets: [
-        ["@babel/preset-env", {useBuiltIns: "usage", corejs: "3.6"}],
+        ["@babel/preset-env", {useBuiltIns: "usage", corejs: 3}],
         "minify"
       ]
     },
     test:{
       presets: [
-        ["@babel/preset-env", {useBuiltIns: "usage", corejs: "3.6", targets: {"node": "current"}}],
-        "minify"
+        ["@babel/preset-env", {useBuiltIns: "usage", corejs: 3, targets: {"node": "current"}}]
       ],
       "plugins": [
         "annotate-console-log"
